dfaf38a2394c523a73c396ddcd75b0601bb6493c
Nicholas Penree <nick@penree.com>
2013-05-20 19:59:51 -0400

    Use Android Studio if available

--- source/50_android.sh
+++ source/50_android.sh
@@ -3,9 +3,16 @@ if [[ "$OSTYPE" =~ "^darwin" ]]; then
   JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home"
   export JAVA_HOME
   
-  ANDROID_HOME="$HOME/Development/adt-bundle-mac-x86_64/sdk"
+  if [ -e "/Applications/Android Studio.app/sdk" ]; then
+    ANDROID_HOME="/Applications/Android Studio.app/sdk"
+  else
+    ANDROID_HOME="$HOME/Development/adt-bundle-mac-x86_64/sdk"
+  fi
+  
   export ANDROID_HOME
   
   PATH=$ANDROID_HOME/tools:$(path_remove $ANDROID_HOME/tools)
   PATH=$ANDROID_HOME/platform-tools:$(path_remove $ANDROID_HOME/platform-tools)
+  
+  export BUILD_MAC_SDK_EXPERIMENTAL=1
 fi
