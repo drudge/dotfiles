c9a886de720f2c883562fb3a0b6a5f97b3ff6561
Nicholas Penree <nick@penree.com>
2013-05-23 23:52:04 -0400

    add linux config for android

--- source/50_android.sh
+++ source/50_android.sh
@@ -1,7 +1,6 @@
 
 if [[ "$OSTYPE" =~ "^darwin" ]]; then
   JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home"
-  export JAVA_HOME
   
   if [ -e "/Applications/Android Studio.app/sdk" ]; then
     ANDROID_HOME="/Applications/Android Studio.app/sdk"
@@ -9,10 +8,13 @@ if [[ "$OSTYPE" =~ "^darwin" ]]; then
     ANDROID_HOME="$HOME/Development/adt-bundle-mac-x86_64/sdk"
   fi
   
-  export ANDROID_HOME
-  
-  PATH=$ANDROID_HOME/tools:$(path_remove $ANDROID_HOME/tools)
-  PATH=$ANDROID_HOME/platform-tools:$(path_remove $ANDROID_HOME/platform-tools)
-  
   export BUILD_MAC_SDK_EXPERIMENTAL=1
+else
+  ANDROID_HOME="/opt/android-studio/sdk"
 fi
+
+export JAVA_HOME
+export ANDROID_HOME
+
+PATH=$ANDROID_HOME/tools:$(path_remove $ANDROID_HOME/tools)
+PATH=$ANDROID_HOME/platform-tools:$(path_remove $ANDROID_HOME/platform-tools)
