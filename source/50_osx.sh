a301a1fb43e4bf8d02e5060f324499e8912c63af
Nicholas Penree <nick@penree.com>
2016-02-22 14:51:57 -0500

    On OSX: Docker improvements
     If a docker machines doesn't exist, ask to create it and then start it up.
    
    If it exists but isn't running, start it up and set the env.
    
    If it is running, just set the env.

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -9,9 +9,28 @@ alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resou
 
 code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*; }
 
-mkdock () {
-  local name=$1
-  docker-machine create --driver vmwarefusion $name --vmwarefusion-memory-size 2048 && eval "$(docker-machine env $name)"
-}
 
-#docker-machine start dev-fusion && eval "$(docker-machine env dev-fusion)"
+local DOCKER_VM_NAME="default"
+local DOCKER_VM_DRIVER="vmwarefusion"
+local DOCKER_VM_EXISTS="$(docker-machine inspect $DOCKER_VM_NAME 2>/dev/null)"
+
+if [[ $DOCKER_VM_EXISTS != "" ]]; then
+  # docker-machine create --driver $DOCKER_VM_DRIVER $DOCKER_VM_NAME
+
+  if [[ "$(docker-machine status $DOCKER_VM_NAME)" != "Running" ]]; then
+    docker-machine start $DOCKER_VM_NAME && eval "$(docker-machine env $DOCKER_VM_NAME)"
+  else
+    eval "$(docker-machine env $DOCKER_VM_NAME)"
+  fi
+else
+  read -q "REPLY?Docker machine named '$DOCKER_VM_NAME' does not exist, create? [y/N] "
+  echo "\n"
+  case $REPLY in
+      [yY][eE][sS]|[yY])
+          docker-machine create --driver "$DOCKER_VM_DRIVER" "$DOCKER_VM_NAME"
+          eval "$(docker-machine env $DOCKER_VM_NAME)"
+          ;;
+      *)
+          ;;
+  esac
+fi
